7f639e10ef9207d7f76d1d21292a88ed4b4eddb4
nikopol-fw <nikolay.dumchev@gmail.com>
2017-10-25 22:14:57 +0600

    Добавляет правки по критериям

--- gulpfile.js
+++ gulpfile.js
@@ -25,6 +25,7 @@ gulp.task("style", function() {
 
     .pipe(minify())
     .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("css"))
     .pipe(gulp.dest("build/css"))
 
     .pipe(server.stream());
